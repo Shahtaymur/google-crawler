fda37a113ab8ca381a54ebcd8d68876297908506
Shah Taymur <49439964+Shahtaymur@users.noreply.github.com>
2021-10-21 07:53:27 +0500

    update proxy config

--- config.py
+++ config.py
@@ -1,10 +1,3 @@
-import requests
-
-username = "geonode_uMZlIrin1i-autoReplace-True"
-password = "b35b456b-90f7-4d88-88c4-b1bae00143a6"
-GEONODE_DNS = "rotating-residential.geonode.com:9000"
-urlToGet = "http://ip-api.com/json"
-proxy = {"http":"http://{}:{}@{}".format(username, password, GEONODE_DNS)}
-r = requests.get(urlToGet , proxies=proxy)
-
-print("Response:\n{}".format(r.text))
+PROXY_USER = ""
+PROXY_PASS = ""
+GEONODE_DNS = ""
